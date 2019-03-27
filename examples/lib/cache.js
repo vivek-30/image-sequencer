fe5b438df6bfa5cc87065007e6c0e27d13495f9f
Harsh Khandeparkar <34770591+HarshKhandeparkar@users.noreply.github.com>
2019-03-27 19:57:42 +0000

    Update cache.js

--- examples/lib/cache.js
+++ examples/lib/cache.js
@@ -17,10 +17,31 @@ var setupCache = function() {
   }
 
   if ('serviceWorker' in navigator) {
-    caches.keys().then(function(cacheNames) {
-      cacheNames.forEach(function(cacheName) {
-        $("#clear-cache").append(" " + cacheName);
-      });
+    if (!('indexedDB' in window)) {
+      console.log('This browser doesn\'t support IndexedDB');
+      caches.keys().forEach(function(cacheName){
+        $('#clear-cache').append(" " + cacheName).fadeIn();
+
+      }
+      return;
+    }
+    var dbPromise = idb.open('cache-db', 1);
+    dbPromise.then(function(db) {
+      var tx = db.transaction('caches', 'readwrite');
+      var store = tx.objectStore('caches');
+      return store.get('new-cache-available');
+    }).then(function(out) {
+      if (out == undefined) return;
+      else {
+        $('#clear-cache').append(`New Cache Available ${out.version}`);
+        var dbPromise = idb.open('cache-db', 1);
+        dbPromise.then(function(db) {
+          var tx = db.transaction('caches', 'readwrite');
+          var store = tx.objectStore('caches');
+          store.delete('new-cache-available');
+          return tx.complete;
+        });
+      }
     });
   }
 
