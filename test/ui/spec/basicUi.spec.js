e994c939ddb7540098eed8579cdc2fa7738ffd9f
Jeffrey Warren <jeff@unterbahn.com>
2019-09-01 16:39:13 -0400

    Update basicUi.spec.js

--- test/ui/spec/basicUi.spec.js
+++ test/ui/spec/basicUi.spec.js
@@ -6,14 +6,14 @@ describe('Default sequencer HTML', function() {
 
   it('adds a step from the quick selector', function() {
     expect($('.step').length).toBe(1);
-    expect($('.step:first img'))[0].src).not.toBeUndefined();
-    expect($('.step:first img'))[0].src).not.toBe('');
+    expect($('.step:first img')[0].src).not.toBeUndefined();
+    expect($('.step:first img')[0].src).not.toBe('');
     // needs more setup
     $("[data-value='brightness']").click()
     expect($('.step').length).toBe(2);
-    expect($('.step:last img'))[0].src).not.toBeUndefined();
-    expect($('.step:last img'))[0].src).not.toBe('');
-    expect($('.step:last img'))[0].src).not.toEqualTo($('.step:first img')[0].src);
+    expect($('.step:last img')[0].src).not.toBeUndefined();
+    expect($('.step:last img')[0].src).not.toBe('');
+    expect($('.step:last img')[0].src).not.toEqual($('.step:first img')[0].src);
     // TODO: test the actual `src` attribute of $('.step:last img') against a known value
   });
 
